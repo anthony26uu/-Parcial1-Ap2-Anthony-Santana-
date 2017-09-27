61fc5838f9a79eb0f11e4899c2d0aba021f78cab
Anthony Javier Santana Polanco <anthony_santana@ucne.edu.do>
2017-09-27 02:05:09 -0400

    Add Script DB

---
+++ [Parcial1-Ap2-Anthony Santana]/App_Data/Scrip.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Presupuestos  (
+    PresupuestoId INT IDENTITY (1, 1) NOT NULL,
+    Fecha           DATETIME NULL,
+    Monto        DECIMAL NULL,
+	Descripcion VARCHAR (80),
+    PRIMARY KEY CLUSTERED (PresupuestoId ASC)
+);
